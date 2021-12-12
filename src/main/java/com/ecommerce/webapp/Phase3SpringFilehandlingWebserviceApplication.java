3f9d811d00ee2a15b93568acefccf00cb85b01f1
Sofia Nayak <nayaksofia1989@gmail.com>
2021-12-13 00:04:23 +0530

    spring file handling

---
+++ src/main/java/com/ecommerce/webapp/Phase3SpringFilehandlingWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phase3SpringFilehandlingWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phase3SpringFilehandlingWebserviceApplication.class, args);
+	}
+
+}
